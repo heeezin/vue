f078bee315354e04780f86734eca50b39d22d65d
leeheezin <gmlwls107@naver.com>
2022-06-11 09:18:12 +0900

    무비

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -3,7 +3,61 @@
   <main id="main">
     <section class="movie__cont">
       <TitleCont name1="Movie" name2="reference" />
-      <div class="movie__inner"></div>
+      <div class="container">
+        <div class="movie__inner">
+          <div class="movie__top">
+            <swiper
+              :slides-per-view="3"
+              :space-between="50"
+              :centeredSlides="true"
+              :pagination="{
+                clickable: true,
+              }"
+              :modules="modules"
+              class="mySwiper"
+              @swiper="onSwiper"
+            >
+              <swiper-slide
+                v-for="(topmovie, index) in topmovies"
+                :key="topmovie.id"
+              >
+                <div class="moive_num">{{ index + 1 }}</div>
+                <img
+                  :src="
+                    'https://image.tmdb.org/t/p/w500/' + topmovie.poster_path
+                  "
+                  :alt="topmovie.title"
+                />
+                <!-- <span class="title">{{ topmovie.title }}</span> -->
+              </swiper-slide>
+              ...
+            </swiper>
+          </div>
+          <div class="movie__search">
+            <form @submit.prevent="SearchMovies()">
+              <label for="search" class="sr-only">검색하기</label>
+              <input
+                type="search"
+                id="search"
+                placeholder="검색하기"
+                v-model="search"
+              />
+              <button type="submit">검색</button>
+            </form>
+          </div>
+          <div class="movie__list">
+            <ul>
+              <li v-for="movie in movies" :key="movie.id">
+                <img
+                  :src="'https:image.tmdb.org/t/p/w500/' + movie.poster_path"
+                  alt=""
+                />
+                <span class="title">{{ movie.title }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
       <ContactCont />
     </section>
   </main>
@@ -15,6 +69,11 @@ import HeaderCont from "@/components/HeaderCont.vue";
 import FooterCont from "@/components/FooterCont.vue";
 import TitleCont from "@/components/TitleCont.vue";
 import ContactCont from "@/components/ContactCont.vue";
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+// Import Swiper styles
+import "swiper/css";
+import { ref } from "vue";
 
 export default {
   components: {
@@ -22,8 +81,190 @@ export default {
     FooterCont,
     TitleCont,
     ContactCont,
+    Swiper,
+    SwiperSlide,
+  },
+
+  setup() {
+    const movies = ref([]);
+    const topmovies = ref([]);
+    const search = ref("spiderman");
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log("slide change");
+    };
+    const TopMovies = () => {
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      };
+
+      fetch(
+        "https://api.themoviedb.org/3/movie/top_rated?api_key=894a080bee34f46fecb98c6f7065d494&language=en-US&page=1",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((data2) => {
+          topmovies.value = data2.results;
+          console.log(data2);
+        })
+        .catch((error) => console.log("error", error));
+    };
+    TopMovies();
+
+    const SearchMovies = () => {
+      var requestOptions = {
+        method: "GET",
+        redirect: "follow",
+      };
+
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=9278d13f704ad0fe53c2263b692efd89&query=${search.value}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          movies.value = data.results;
+          search.value = "";
+          console.log(data);
+        })
+        .catch((error) => console.log("error", error));
+    };
+    SearchMovies();
+
+    return {
+      movies,
+      topmovies,
+      search,
+      SearchMovies,
+      onSwiper,
+      onSlideChange,
+    };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.movie__top {
+  margin-bottom: 10%;
+
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    padding-top: 90px;
+    text-align: center;
+    font-size: 18px;
+    position: relative;
+    /* Center slide text vertically */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+
+    .moive_num {
+      font-family: var(--main_font);
+      font-size: 80px;
+      font-weight: 700;
+      position: absolute;
+      left: -30px;
+      top: 26px;
+    }
+    img {
+      position: relative;
+      z-index: 10;
+      width: 150px;
+      height: 300px;
+    }
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .num {
+    font-size: 10vw;
+    line-height: 1;
+    position: absolute;
+    color: #ccc;
+    margin-top: -140px;
+    z-index: 6;
+  }
+
+  .title {
+    font-family: var(--subKor_font);
+    font-size: 2vw;
+  }
+}
+
+.movie__search {
+  position: relative;
+
+  input {
+    border: 2px solid var(--light_border);
+    width: 100%;
+    background: var(--black);
+    border-radius: 50px;
+    padding: 1rem 3rem 1rem 2rem;
+    color: var(--light_bg);
+    font-family: var(--subKor_font);
+    margin-bottom: 3%;
+  }
+  button {
+    position: absolute;
+    right: 10px;
+    top: 9px;
+    background: transparent;
+    border: 0;
+    color: var(--black);
+    background: var(--white);
+    font-family: var(--subKor_font);
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    font-size: 12px;
+    transition: opacity 0.3 ease;
+
+    &:active {
+      opacity: 0.7;
+    }
+  }
+}
+.movie__cont {
+  background: var(--dark_bg);
+}
+.movie__list {
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    gap: 1%;
+
+    li {
+      width: 24%;
+
+      .title {
+        font-size: 16px;
+        font-family: var(—subKor_font);
+        display: block;
+        padding: 2% 0;
+        margin-bottom: 3%;
+      }
+    }
+  }
+}
+</style>
